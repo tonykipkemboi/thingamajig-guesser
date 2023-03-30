6aa9c84a52d47ec2db1efeab9748f21b6ce1c920
Tony Kipkemboi <tonykip@seas.upenn.edu>
2023-03-30 10:30:02 -0400

    Create model_utils.py

---
+++ app_utils/model_utils.py
@@ -0,0 +1,11 @@
+import torch
+
+def load_yolov5_model() -> object:
+    """
+    Load the pre-trained YOLOv5 model using the torch.hub utility.
+    This function is cached to avoid re-loading the model each time.
+    Returns:
+        object: The loaded YOLOv5 model.
+    """
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    return model
